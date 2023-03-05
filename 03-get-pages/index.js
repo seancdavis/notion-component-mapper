434120df688edd496094aab3753994ec7e350a45
Sean C Davis <scdavis41@gmail.com>
2023-03-05 07:27:11 -0500

    Add the export script

--- 03-get-pages/index.js
+++ 03-get-pages/index.js
@@ -1,27 +1,39 @@
 #!/usr/bin/env node
 
 const { Client } = require("@notionhq/client");
+const fs = require("fs");
+const path = require("path");
 
 // Sensitive data is stored in .env file
 require("dotenv").config();
 
-// Exit if no query is provided
-const query = process.argv[2];
-if (!query) {
-  console.log("Please provide a query");
-  process.exit(1);
-}
-
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-async function getDatabaseId() {
-  const response = await notion.search({
-    query,
-    filter: { property: "object", value: "database" },
-  });
-  response.results.map((result) =>
-    console.log(`${result.title[0].plain_text} -> ${result.id}`)
-  );
+async function getBlocks(block_id) {
+  let { results: children } = await notion.blocks.children.list({ block_id });
+  for (const child of children) {
+    const grandchildren = await getBlocks(child.id);
+    child.children = grandchildren;
+  }
+  return children;
 }
 
-getDatabaseId();
+async function importPages() {
+  // Retrieve pages from the database.
+  let { results: pages } = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+
+  // Attach blocks to pages
+  for (const page of pages) {
+    const blocks = await getBlocks(page.id);
+    page.children = blocks;
+  }
+
+  // Write the result to file.
+  const outputFile = path.join(__dirname, "notion-export.json");
+  fs.writeFileSync(outputFile, JSON.stringify(pages, null, 2));
+  console.log(`Wrote ${pages.length} pages to ${outputFile}`);
+}
+
+importPages();
